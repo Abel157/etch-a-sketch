3348e76e32191946c3a4f9ae9593a13736e0e9bc
Abel157 <abelsewinet1213@gmail.com>
2024-09-08 21:34:39 +0300

    fix the container's width and height

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const container=document.querySelector(".container");
+
 function drawGrid(n){
-    let no=0;
+    const cellSize = 600/n;
+    console.log(cellSize);
     for (let i=0; i<n;i++){
         const holder=document.createElement("div");//needs to be created again and again to be appended
         for(let j=0;j<n;j++){
             const grid=document.createElement("div");
             holder.className="holder";
-            grid.className="grid";
-            grid.addEventListener('mouseenter',(e)=>{
-                grid.setAttribute("style","background-color:green;");
-            });
+            grid.classList="grid";
+            grid.style.width=cellSize+"px";
+            grid.style.height=cellSize+"px";
             holder.appendChild(grid);
+            grid.addEventListener('mouseenter',(e)=>{
+                grid.style.backgroundColor="green";
+            });
+           
         };
         container.appendChild(holder);
-
-}
+    
+    };
+    
 };
 
 
-drawGrid(6)
+drawGrid(100);
 
