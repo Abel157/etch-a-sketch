3697a8c0fe6346d93bc7426784ed28038503f23f
Abel157 <abelsewinet1213@gmail.com>
2024-09-09 07:01:28 +0300

    add the functionality to generate new sketch

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const container=document.querySelector(".container");
-
-function drawGrid(n){
+const btn=document.querySelector("button");
+function drawGrid(n=16){
     const cellSize = 600/n;
     console.log(cellSize);
     for (let i=0; i<n;i++){
         const holder=document.createElement("div");//needs to be created again and again to be appended
+        container.appendChild(holder);
         for(let j=0;j<n;j++){
             const grid=document.createElement("div");
             holder.className="holder";
@@ -12,17 +13,22 @@ function drawGrid(n){
             grid.style.width=cellSize+"px";
             grid.style.height=cellSize+"px";
             holder.appendChild(grid);
+            
             grid.addEventListener('mouseenter',(e)=>{
                 grid.style.backgroundColor="green";
             });
            
         };
-        container.appendChild(holder);
+        
+        
     
     };
-    
 };
+btn.addEventListener('click',()=>{
+    const n=prompt("what size per side");
+    container.replaceChildren()
+    drawGrid(n);
+});
+drawGrid(16);
 
-
-drawGrid(100);
 
